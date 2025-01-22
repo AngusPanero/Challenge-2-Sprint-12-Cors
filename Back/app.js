c07d48dcf0ee93a821ab900834368bf82d9ad8db
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2025-01-22 19:08:56 -0300

    BACK TERMINADO

---
+++ Back/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const PORT = 4001;
+
+app.get("/", async (req, res) => {
+    const url = `https://rickandmortyapi.com/api/character`
+    
+    try{
+        const response = await axios.get(url)
+        const personajes = response.data.results;
+        const todosLosPersonajes = personajes.map(({ id, image, name, status, species, gender }) => 
+            ({ id, image, name, status, species, gender }))
+
+        res.json(todosLosPersonajes);
+
+    } catch(error){
+        res.status(404).json({ mensaje: `404 - Personaje No Encontrado`})
+    }
+})
+
+app.get("/:personaje", async (req, res) => {
+    const nombrePersonaje = decodeURIComponent(req.params.personaje)
+    const url2 = `https://rickandmortyapi.com/api/character?name=${nombrePersonaje}`
+
+    try{
+        const response = await axios.get(url2)
+        const personaje = response.data.results
+        res.json({ personaje })
+
+    } catch(error){
+        res.status(404).json({ Mensaje: `Personaje No Encontrado` })
+    }
+})
+
+app.use((req, res) => {
+    res.send(`<h1>404 - Página No Encontrada</h1>`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Listening On Port http://localhost:${PORT}`)
+})
